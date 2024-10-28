68cd7ff7573ef1c9b7543fbca1d7b3bb2d73b84f
ShakirMahmud <shakirmahmud50@gmail.com>
2024-10-28 19:39:00 +0600

    using axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import LineChart from './components/LineChart/LineChart'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       {/* <DaisyNav></DaisyNav> */}
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
